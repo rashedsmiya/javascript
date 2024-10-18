a8b4f8ef4ed234dec7acdce7a8ce538e3ed394ef
rashedsmiya <rashedmiyaofficial@gmail.com>
2024-10-18 19:11:51 +0600

    datatype oparasions

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -1,27 +1,27 @@
-// let score = 'true'
+let score = 'true'
 
-// console.log(typeof score);
-// console.log(typeof (score));
+console.log(typeof score);
+console.log(typeof (score));
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
-// '33' => 33
-// '33abc' => Nan
-// true => 1; false => 0
+'33' => 33
+'33abc' => Nan
+true => 1; false => 0
 
-// let isLoggedIn = ""
+let isLoggedIn = ""
 
-// let booleanIsLoggdIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggdIn);
+let booleanIsLoggdIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggdIn);
 
-// 1 => true
-// 0 => false 
-// "" => false 
-// "hitesh" => true
+1 => true
+0 => false 
+"" => false 
+"hitesh" => true
 
-// let someNumber = 33
-// let stringNumber = String(someNumber);
-// console.log(stringNumber);
-// console.log(typeof stringNumber); 
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber); 
