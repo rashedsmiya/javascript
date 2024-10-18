7f24cec1dfb5fd7bef8c9608837a3e4fb4839829
rashedsmiya <rashedmiyaofficial@gmail.com>
2024-10-18 16:38:11 +0600

    add allfile

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,10 +1,38 @@
-const accoubId = 144553
-let accountEmail = "rashedmiyaofficial@gmail.com";
-var accountPassword = '12345';
-accoubCity = "Dhaka";
 
-accountEmail = "hc@hc.com";
-accountPassword = "21212121"
-accoubCity = "Bengaluru";
+// function myvar(){
+//    var a = 10;
+//    if(true) {
+//       var a = 20;
+//       console.log(a + "\n");
+//    }
+//    console.log(a + "\n");
+// }
 
-console.log();
+// myvar(); 
+
+// function mylet(){
+//    let a = 70;
+//    if(true) {
+//       let a = 90;
+//       console.log(a + "\n");
+//    }
+//    console.log(a + "\n"); 
+// }
+
+// mylet(); 
+
+// const secondsInminit = 60;
+
+// secondsInminit = 30; // On time user
+
+// console.log(secondsInminit);
+
+
+// var language = 'javascript';
+
+// if (true) {
+//    const age = 25; /// block scope
+// }
+// console.log(age); 
+
+
