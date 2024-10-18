d6ac0f9c4ba1a34e4951dcb22b8691c525702d93
rashedsmiya <rashedmiyaofficial@gmail.com>
2024-10-18 18:26:54 +0600

    firsdata2

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,13 +2,13 @@
 
 // alert(3 + 3); // we are using  nodejs, not browser
 
-console.log(4+4);  // code readability should be high
-console.log("Hitesh");
+// console.log(4+4);  // code readability should be high
+// console.log("Hitesh");
 
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
-let state = null
+// let name = "hitesh"
+// let age = 18
+// let isLoggedIn = false
+// let state = null
 
 // number => 2 to power 53
 // bigint 
@@ -21,6 +21,24 @@ let state = null
 // object
 
 
-console.log(typeof undefined); // undefined
+// console.log(typeof undefined); // undefined
 
-console.log(typeof null); // Object
+// console.log(typeof null); // Object
+
+
+// let age = 25;
+// age = "TwentyFive";
+// console.log(age);
+
+
+// int age = 25;
+// Primitive data types
+
+// 1.Number
+// let age = 25;
+// let temp = -25;
+// let price = 50.50;
+
+// 2. String 
+// let name = 'Rakesh';
+
