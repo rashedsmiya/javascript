32b1cd41a015e2acaba56887bc6e304b14f3ce23
rashedsmiya <rashedmiyaofficial@gmail.com>
2024-10-18 20:20:08 +0600

    commit add once data

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,34 @@
+// Primitive 
+
+// 7 types : String, Numberm Boolearn, null, undefined, Symbol, BigInt
+
+// const score = 100 
+// const scoreValue = 100.3 
+
+// const isLoggedIn = false
+// const outsideTemp = null 
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3456535766585
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions 
+
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//    name: "hitesh",
+//    age : 22, 
+
+// }
+// const myFunction = function(){
+//    console.log("Hello WOrld");
+// } 
+
+// console.log(typeof bigNumber);
+
