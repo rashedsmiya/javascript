a906670a5a3a59ba92ef41db71bdc774f97f6c48
rashedsmiya <rashedmiyaofficial@gmail.com>
2024-10-18 14:40:44 +0600

    ass

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accoubId = 144553
+let accountEmail = "rashedmiyaofficial@gmail.com";
+var accountPassword = '12345';
+accoubCity = "Dhaka";
+
+accountEmail = "hc@hc.com";
+accountPassword = "21212121"
+accoubCity = "Bengaluru";
+
+console.log();
