f7f9892beec824cd06ba3d1e444ec721221f1bf7
rashedsmiya <rashedmiyaofficial@gmail.com>
2024-10-19 06:28:02 +0600

    number2

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,32 +1,48 @@
-const name = "hitesh"
-const repoCount = 50
+// const name = "hitesh"
+// const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hiteshhc');
+// const gameName = new String('hiteshhc');
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t')); 
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t')); 
 
-
-const anotherString = gameName.slice(-8, 4);
-console.log(anotherString);
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
 
 
-const newStringOne = " hitesh " 
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// const anotherString = gameName.slice(-8, 4);
+// console.log(anotherString);
 
-const url = "https:://hitesh.com"
-console.log(url.repeat('%20', '-'));
-console.log(gameName.split);
+
+// const newStringOne = " hitesh " 
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https:://hitesh.com"
+// console.log(url.repeat('%20', '-'));
+// console.log(gameName.split);
+
+
+var str = "Javascript is        S great language";
+// var a = str.length;
+// var a = str.toLowerCase();
+// var a = str.includes("great");
+// var a = str.startsWith("J");
+// var a = str.endsWith("language");
+// var a = str.search("great");
+// var a = str.search("great");
+// var a = str.match("great");
+// var a = str.replace("is", "PHP");
+var a = str.trim("is");
+
+console.log(a);
