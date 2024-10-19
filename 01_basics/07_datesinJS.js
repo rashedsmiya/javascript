45cd8dfdd51500f61790b8ad1327e1d200c19d27
rashedsmiya <rashedmiyaofficial@gmail.com>
2024-10-19 08:07:19 +0600

    datetime

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,42 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23);
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3);
+// let myCreatedDate = new Date("2023-01-14");
+// let myCreatedDate = new Date("01-14-2023");
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now() 
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime);
+// console.log(Math.floor(Date.now()/1999));
+
+// let newDate = new Date()
+// console.log(newDate);
+
+// let newDate = new Date(3020, 3, 5, 8, 2, 34);
+// console.log(newDate);
+
+// let yr = newDate.getFullYear();
+// console.log("The year is ", yr);
+
+// let dat = newDate.getDate();
+// console.log("The date is ", dat);
+
+// let month = newDate.getMonth();
+// console.log("The Month is ", month);
+
+// let hours = newDate.getHours();
+// console.log("The hours is", hours);
+
+// newDate.setDate(8);
+// newDate.setMinutes(29); 
+// console.log(newDate);
+
