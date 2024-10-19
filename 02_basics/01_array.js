ad4956b3c0fb3f0cbd647c4422330b4c443a38ec
rashedsmiya <rashedmiyaofficial@gmail.com>
2024-10-19 10:54:09 +0600

    addarray

---
+++ 02_basics/01_array.js
@@ -0,0 +1,38 @@
+// array 
+
+// const myArr = [0,1,2,3,4,5]
+// const myHeors = ["shaktiman", "naagraj"]
+
+// const myArr2 = new Array(1,2,3,4);
+// console.log(myArr[0]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9);
+// myArr.shift(9);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+ // slice, splice
+
+//  console.log("A ", myArr);
+
+//  const myn1 = myArr.slice(1, 3);
+
+//  console.log(myn1);
+//  console.log("B", myArr);
+
+// let selectedColor = ['red', 'blue'];
+// selectedColor[2] = 1;
+// console.log(selectedColor.length);
+
