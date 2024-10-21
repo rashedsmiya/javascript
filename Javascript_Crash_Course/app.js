954f0d0cd12eb6650abbb1cca5b207a7bac9a36b
rashedsmiya <rashedmiyaofficial@gmail.com>
2024-10-21 17:40:00 +0600

    app,js

--- Javascript_Crash_Course/app.js
+++ Javascript_Crash_Course/app.js
@@ -15,4 +15,68 @@
 
 // console.log("Person Name " +name+ " Person cell " + cell + email + website);
 
+//1.Arithmetic Operators (+,-,*,%,++,--)
+// 2. Assigment Operators (=,+=,-=,*=,/=,%=)
+// 3. Comparison Operators (==,===,!=,!==,>,<,>=,<=,?)
+// 4. Logical Operators ($$,||,!)
+
+
+// Number string Object, Boolean Array, 
+
+// let product_price = 1000;
+// let product_qty = 21;
+// let product_name = "AC";
+
+// let total_price = product_price * product_qty;
+
+// let per_payment = 5;
+
+// let per_payment_amount=total_price/per_payment;
+
+// alert(per_payment_amount);
+ 
+// var age = 12;
+// if(age < 18){
+//    console.log(age);
+// }
+
+// var course_fee = 3000;
+// if(course_fee!=3000){
+//     document.getElementById("place").innerHTML="Sorry course fee must be 300";
+// }
+// else{
+//    document.getElementById("place").innerHTML= "Welcome to mamurjor";
+// }
+
+
+// var full_stack = "php";
+// var module_two = "webdesign";
+// var module_three = "oop using php";
+
+// if(full_stack=="php" && module_two=="webdesign"){
+//    document.getElementById("place").innerHTML= "sorry only php";
+// }
+
+// var name = 'Mamurjor';
+// var cell = 5+5;
+// document.getElementById("place").innerHTML=;
+
+// var course_fee = {
+//    "webdesign" : 3000,
+//    "Theme Customization" : 2500
+// }
+
+// var course_fee = {
+//    "webdesign" : 3000,
+//    "Theme_Customization" : 2500,
+//    "php" : 1000,
+//    "theme_development":3200,
+//    "laravel" : 12000
+// }
+
+// var classes = document.getElementsByTagName("h2");
+// document.querySelector("h1").style.color="red";
+
+
+// classes[0].innerHTML=course_fee.laravel;
 
---
+++ Javascript_Crash_Course/functions.js
@@ -0,0 +1,67 @@
+// function get_result(){
+//    var get_value = document.getElementById("get").value;
+
+// if(get_value>=33 && get_value<=39){
+//    document.getElementById("set").innerHTML="D";
+// }
+// else if(get_value>=40 && get_value<=49){
+//    document.getElementById("set").innerHTML="C";
+// }
+// else if(get_value>=50 && get_value<=59){
+//    document.getElementById("set").innerHTML="B";
+// }
+// else if(get_value>=60 && get_value<=69){
+//    document.getElementById("set").innerHTML="A-";
+// }
+// else if(get_value>=70 && get_value<=79){
+//    document.getElementById("set").innerHTML="B";
+// }
+// else if(get_value>=80 && get_value<=100){
+//    document.getElementById("set").innerHTML="B";
+// }
+// else {
+//    document.getElementById("set").innerHTML="F";
+// }
+// }
+
+function get_address(){
+   alert('Mirpur',837637568);
+}
+
+function get_result(){
+   var get_value = document.getElementById("get").value;
+
+if(get_value>=33 && get_value<=39){
+   document.getElementById("set").innerHTML="D";
+} 
+
+else if(get_value>=40 && get_value<=49){
+   document.getElementById("set").innerHTML="C";
+}
+
+else if(get_value>=50 && get_value<=59){
+   document.getElementById("set").innerHTML="B";
+}
+
+else if(get_value>=60 && get_value<=69){
+   document.getElementById("set").innerHTML="A-";
+}
+
+else if(get_value>=70 && get_value<=79){
+   document.getElementById("set").innerHTML="B";
+}
+
+else if(get_value>=80 && get_value<=100){
+   document.getElementById("set").innerHTML="B";
+}
+
+else {
+   document.getElementById("set").innerHTML="F";
+ }
+}
+
+function set_color(){
+   document.getElementById("set").style.color="green";
+}
+
+
