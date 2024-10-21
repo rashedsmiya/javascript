0e35757c24aefd4dd2c0b09cbb67a6598a237d1b
rashedsmiya <rashedmiyaofficial@gmail.com>
2024-10-21 11:43:24 +0600

    js all file add

---
+++ Javascript_Crash_Course/app.js
@@ -0,0 +1,18 @@
+// var first_name = "hadi"
+// var last_name = "jaman"
+
+
+// alert(first_name+last_name); 
+
+// var 
+// let 
+// const 
+
+// name = "hadi jaman";
+// cell = "0987654321";
+// email = " info@gmail.com";
+// website = "mamurjor.com";
+
+// console.log("Person Name " +name+ " Person cell " + cell + email + website);
+
+
